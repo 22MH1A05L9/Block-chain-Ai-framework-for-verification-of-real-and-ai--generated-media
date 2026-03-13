901a770ec1976372f8e67951a6a89b80b97ce5f3
unknown <22MH1A05L9@acoe.edu.in>
2026-03-13 18:09:22 +0530

    Refresh

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -14,3 +14,7 @@ app.use("/api/media", mediaRoutes);
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully");
+});
