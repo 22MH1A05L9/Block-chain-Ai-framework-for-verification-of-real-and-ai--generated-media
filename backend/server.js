e787d27343b9d4d68397f497fe0a99f69f82c0f5
unknown <22MH1A05L9@acoe.edu.in>
2026-03-13 18:26:55 +0530

    Refresh

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,24 @@
+// const express = require("express");
+// const cors = require("cors");
+// require("dotenv").config();
+
+// const mediaRoutes = require("./routes/mediaRoutes");
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api/media", mediaRoutes);
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on port ${process.env.PORT}`);
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Backend is running successfully");
+// });
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -11,10 +32,12 @@ app.use(express.json());
 
 app.use("/api/media", mediaRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
-
 app.get("/", (req, res) => {
   res.send("Backend is running successfully");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
